3dec5030dd204d1c3ee5d05da705dbf34ff1483f
Julia Ayubova <ayubova.yuliya@gmail.com>
2023-03-01 17:36:07 +0300

    add search

--- components/common/nav/AdminSecondaryNav.tsx
+++ components/common/nav/AdminSecondaryNav.tsx
@@ -1,30 +1,35 @@
-import { FC } from 'react';
-import { useRouter } from 'next/router';
-import { signOut } from 'next-auth/react';
+import { FC } from "react";
+import { useRouter } from "next/router";
+import { signOut } from "next-auth/react";
 
-import Dropdown, { DropdownOptions } from '../Dropdown';
-import ProfileHead from '../ProfileHead';
-import SearchBar from '../SearchBar';
-import useDarkMode from 'hooks/useDarkMode';
+import Dropdown, { DropdownOptions } from "../Dropdown";
+import ProfileHead from "../ProfileHead";
+import SearchBar from "../SearchBar";
+import useDarkMode from "hooks/useDarkMode";
 
 interface Props {}
 
-const AdminSecondaryNav: FC<Props> = (props): JSX.Element => {
+const AdminSecondaryNav: FC<Props> = (): JSX.Element => {
   const router = useRouter();
   const { toggleTheme } = useDarkMode();
 
-  const navigateToCreateNewPost = () => router.push('admin/posts/create');
-  const handleLogout = async () => await signOut();
+  const navigateToCreateNewPost = () => router.push("admin/posts/create");
+
+  const handleSearchSubmit = (query: string) => {
+    if (!query.trim()) return;
+    router.push(`/admin/posts/search?title=${query}`);
+  };
 
   const options: DropdownOptions = [
-    { label: 'Add new post', onClick: navigateToCreateNewPost },
-    { label: 'Change theme', onClick: toggleTheme },
-    { label: 'Log out', onClick: handleLogout },
+    { label: "Add new post", onClick: navigateToCreateNewPost },
+    { label: "Change theme", onClick: toggleTheme },
+    { label: "Log out", onClick: signOut },
   ];
   return (
     <div className="flex items-center justify-between">
-      <SearchBar />
+      <SearchBar onSubmit={handleSearchSubmit} />
       <Dropdown head={<ProfileHead nameInitial="J" />} options={options} />
+      {/** TODO: Add avatar name from user (useAuth) */}
     </div>
   );
 };
---
+++ pages/admin/posts/search/index.tsx
@@ -0,0 +1,60 @@
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useCallback, useState } from "react";
+import axios from "axios";
+
+import AdminLayout from "components/layout/AdminLayout";
+import InfiniteScrollPosts from "components/common/InfiniteScrollPosts";
+import { filterPosts } from "utils/helper";
+
+import { PostDetail } from "types";
+
+interface Props {}
+
+const Search: NextPage<Props> = () => {
+  const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState<PostDetail[]>([]);
+
+  const { query } = useRouter();
+  const title = query.title;
+
+  const handleSearch = useCallback(async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios(`/api/posts/search?title=${title}`);
+      setLoading(false);
+      setResults(data.results);
+    } catch (error: any) {
+      console.error(error.message);
+      setLoading(false);
+    }
+  }, [title]);
+
+  useEffect(() => {
+    if (loading) return;
+    handleSearch();
+  }, [title]);
+
+  return (
+    <AdminLayout>
+      {loading && <h1> Searching...</h1>}
+      {!!results.length && !loading && (
+        <InfiniteScrollPosts
+          hasMore={false}
+          next={() => {}}
+          dataLength={results.length}
+          posts={results}
+          showControls={true}
+          onPostRemoved={(post) => setResults(filterPosts(results, post))}
+        />
+      )}
+      {!results.length && !loading && (
+        <h1 className="text-center text-3xl font-semibold opacity-40 text-secondary-dark">
+          Not found :(
+        </h1>
+      )}
+    </AdminLayout>
+  );
+};
+
+export default Search;
