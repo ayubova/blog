73b8115f0d2d685483ec4cc91f58412eb0c415c0
Julia Ayubova <ayubova.yuliya@gmail.com>
2023-03-28 15:05:38 +0300

    add about me info

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import type { NextPage } from "next";
 import Image from "next/image";
 import ContactButtons from "components/common/ContactButtons";
