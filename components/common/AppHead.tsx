51fb247ee20504bef24df6e5fa96da9eb9e330b9
Julia Ayubova <ayubova.yuliya@gmail.com>
2023-04-03 16:45:53 +0300

    feat: add meta description

--- components/common/AppHead.tsx
+++ components/common/AppHead.tsx
@@ -8,11 +8,15 @@ interface Props {
 
 export const APP_NAME = "Julia Ayubova";
 
+//TODO add desc from props or delete prop
+const defaultDesc =
+  "Welcome to my personal blog! Here you'll find articles on topics ranging from web technology and to personal development and lifestyle. Join me on my journey and let's explore the world together.";
+
 const AppHead: FC<Props> = ({ title, desc }): JSX.Element => {
   return (
     <Head>
       <title>{title ? title + " | " + APP_NAME : APP_NAME}</title>
-      <meta content={desc} name="description" />
+      <meta content={desc ? desc : defaultDesc} name="description" />
     </Head>
   );
 };
