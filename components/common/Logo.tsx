63feb0483350cbb0f60c946374e3138c2707c244
Ayubova <ayubova.yuliya@gmail.com>
2023-08-03 13:07:02 +0400

    WIP redesign

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import logo from "../../public/logo_1.png";
 
 const Logo: FC = () => (
-  <Image src={logo} height="50px" width="300px" alt="logo" />
+  <Image src={logo} height={50} width={300} alt="logo" />
 );
 
 export default Logo;
