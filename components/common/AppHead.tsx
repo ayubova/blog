0f4177ffa89d8dc750fc5f9bdcedbbc036fb005a
Ayubova <ayubova.yuliya@gmail.com>
2023-08-09 15:01:09 +0400

    add meta tags

--- components/common/AppHead.tsx
+++ components/common/AppHead.tsx
@@ -16,8 +16,24 @@ const AppHead: FC<Props> = ({title, desc}): JSX.Element => {
   return (
     <Head>
       <title>{title ? title + " | " + APP_NAME : APP_NAME}</title>
-      <meta content={desc ? desc : defaultDesc} name="description" />
+      <meta property="og:title" content={title ? title + " | " + APP_NAME : APP_NAME}></meta>
+      <meta property="og:description" content={desc ? desc : defaultDesc}></meta>
+      <meta property="og:url" content="https://www.ayubova.com/"></meta>
+      <meta property="og:site_name" content="Julia Ayubova"></meta>
+      <meta property="og:locale" content="en-US"></meta>
+      <meta property="og:image:type" content="image/jpeg"></meta>
+      <meta property="og:image:width" content="1920"></meta>
+      <meta property="og:image:height" content="1080"></meta>
       <meta property="og:image" content="https://res.cloudinary.com/dynf9cvqt/image/upload/v1691572509/ayubova_meta.png" />
+      <meta property="og:type" content="website"></meta>
+      <meta name="twitter:card" content="summary_large_image"></meta>
+      <meta property="twitter:title" content={title ? title + " | " + APP_NAME : APP_NAME}></meta>
+      <meta property="twitter:description" content={desc ? desc : defaultDesc}></meta>
+      <meta property="twitter:site_name" content="Julia Ayubova"></meta>
+      <meta property="twitter:image:type" content="image/jpeg"></meta>
+      <meta property="twitter:image:width" content="1920"></meta>
+      <meta property="twitter:image:height" content="1080"></meta>
+      <meta property="twitter:image" content="https://res.cloudinary.com/dynf9cvqt/image/upload/v1691572509/ayubova_meta.png" />
     </Head>
   );
 };
