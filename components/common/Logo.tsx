34acbe1f70027ac7d9b514491d3f78feef1b022a
Ayubova <ayubova.yuliya@gmail.com>
2023-08-10 16:49:11 +0400

    add menu hide on scroll

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 import logo from "../../public/logo_1.png";
 
 type Props = {
-  menuVisible: boolean
+  menuVisible?: boolean
 }
 
-const Logo: FC<Props> = ({menuVisible}) => (
+const Logo: FC<Props> = ({menuVisible = true}) => (
   <Image src={logo} height={50} width={menuVisible ? 300 : 200} alt="logo" className="transition-all duration-1500"/>
 );
 
