f888d2a0dbd522d7fee26cc4af570acf75a09b06
Ayubova <ayubova.yuliya@gmail.com>
2023-09-12 12:19:08 +0400

    fix width

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -91,7 +91,7 @@ const PostPage: NextPage<Props> = ({post, tagsList}) => {
 
   return (
     <DefaultLayout title={title} desc={meta} tags={tagsList} metaSrc={thumbnail} showTopButton>
-      <div className="px-5 w-full lg:max-w-4xl m-auto">
+      <div className="px-5 w-full lg:max-w-4xl m-auto max-w-[100vw]">
 
         <div className="flex items-center space-x-2 pb-10 justify-center">
           {tags.map((tagItem, index) => (
