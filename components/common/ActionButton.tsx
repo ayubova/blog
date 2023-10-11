b287b42c67080794d912c3ef687d87cdc469baa4
Ayubova <ayubova.yuliya@gmail.com>
2023-10-11 15:57:15 +0400

    add tooltip button

--- components/common/ActionButton.tsx
+++ components/common/ActionButton.tsx
@@ -1,11 +1,13 @@
 import {FC, MouseEventHandler} from "react";
 import {BiLoader} from "react-icons/bi";
-
+import {Tooltip} from "react-tippy";
 interface Props {
   title: string;
   busy?: boolean;
   disabled?: boolean;
   onClick?: MouseEventHandler<HTMLButtonElement>;
+  icon?: React.ReactElement,
+  tip?: string
 }
 
 const ActionButton: FC<Props> = ({
@@ -13,14 +15,39 @@ const ActionButton: FC<Props> = ({
   busy = false,
   title,
   onClick,
+  icon,
+  tip
 }): JSX.Element => {
-  return (
+  return tip ? (
+    <Tooltip
+      title={tip}
+      position="bottom"
+      trigger={"mouseenter"}
+      theme="light"
+      arrow
+    >
+      <button
+        className="text-primary-light bg-secondary-dark drop-shadow-sm px-6 py-2 hover:scale-[0.97] duration-100 rounded w-full flex items-center justify-center space-x-2 transition disabled:opacity-50 disabled:hover:scale-100"
+        onClick={onClick}
+        disabled={disabled}
+      >
+        <div className="flex gap-x-4 justify-between items-center">
+          {icon ?  icon : null}
+          <span>{title}</span>
+        </div>
+        {busy && <BiLoader className="animate-spin" size={20} />}
+      </button>
+    </Tooltip>
+  ) : (
     <button
-      className="text-primary-light bg-secondary-dark drop-shadow-sm px-6 py-2 hover:scale-[0.97] duration-100 rounded w-full flex items-center justify-center space-x-2 transition"
+      className="text-primary-light bg-secondary-dark drop-shadow-sm px-6 py-2 hover:scale-[0.97] duration-100 rounded w-full flex items-center justify-center space-x-2 transition disabled:opacity-50 disabled:hover:scale-100"
       onClick={onClick}
       disabled={disabled}
     >
-      <span>{title}</span>
+      <div className="flex gap-x-4 justify-between items-center">
+        {icon ?  icon : null}
+        <span>{title}</span>
+      </div>
       {busy && <BiLoader className="animate-spin" size={20} />}
     </button>
   );
