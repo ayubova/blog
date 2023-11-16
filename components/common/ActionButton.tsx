410586ac9084455c99fb7f1b5113dea7a0a02e00
Ayubova <ayubova.yuliya@gmail.com>
2023-11-16 15:29:30 +0400

    fix typescript error

--- components/common/ActionButton.tsx
+++ components/common/ActionButton.tsx
@@ -1,13 +1,14 @@
-import {FC, MouseEventHandler} from "react";
+import React, {FC, MouseEventHandler} from "react";
 import {BiLoader} from "react-icons/bi";
 import {Tooltip} from "react-tippy";
+
 interface Props {
   title: string;
   busy?: boolean;
   disabled?: boolean;
   onClick?: MouseEventHandler<HTMLButtonElement>;
-  icon?: React.ReactElement,
-  tip?: string
+  icon?: React.ReactElement;
+  tip?: string;
 }
 
 const ActionButton: FC<Props> = ({
@@ -16,42 +17,34 @@ const ActionButton: FC<Props> = ({
   title,
   onClick,
   icon,
-  tip
+  tip,
 }): JSX.Element => {
-  return tip ? (
-    <Tooltip
-      title={tip}
-      position="bottom"
-      trigger={"mouseenter"}
-      theme="light"
-      arrow
-      html={(
-        <button
-          className="text-primary-light bg-secondary-dark drop-shadow-sm px-6 py-2 hover:scale-[0.97] duration-100 rounded w-full flex items-center justify-center space-x-2 transition disabled:opacity-50 disabled:hover:scale-100"
-          onClick={onClick}
-          disabled={disabled}
-        >
-          <div className="flex gap-x-4 justify-between items-center">
-            {icon ?  icon : null}
-            <span>{title}</span>
-          </div>
-          {busy && <BiLoader className="animate-spin" size={20} />}
-        </button>
-      )}
-    /> 
-  ) : (
+  const Button = () => (
     <button
       className="text-primary-light bg-secondary-dark drop-shadow-sm px-6 py-2 hover:scale-[0.97] duration-100 rounded w-full flex items-center justify-center space-x-2 transition disabled:opacity-50 disabled:hover:scale-100"
       onClick={onClick}
       disabled={disabled}
     >
       <div className="flex gap-x-4 justify-between items-center">
-        {icon ?  icon : null}
+        {icon ? icon : null}
         <span>{title}</span>
       </div>
       {busy && <BiLoader className="animate-spin" size={20} />}
     </button>
   );
+
+  return tip ? (
+    <Tooltip
+      title={tip}
+      position="bottom"
+      trigger={"mouseenter"}
+      theme="light"
+      arrow>
+      <Button />
+    </Tooltip>
+  )
+    : <Button />;
 };
 
 export default ActionButton;
+
