cee6f4b0871b05c059160b97059ba9a1454009bc
Ayubova <ayubova.yuliya@gmail.com>
2024-04-03 18:48:06 +0400

    upgrade next js version

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,70 @@
 import PostContent from "components/common/PostContent";
 import dbConnect from "lib/dbConnect";
-import {getPost} from "api";
 import Post from "models/Post";
+import {PostContent as PostContentInterface} from 'types'
+
+export const getPost = async (params: { slug: string }): Promise<PostContentInterface | null> => {
+  try {
+    await dbConnect();
+    const post = await Post.findOne({slug: params?.slug});
+
+    if (!post) {
+      return null;
+    }
+
+    if (process.env.NODE_ENV !== "development") {
+      post.views = (post.views ?? 0) + 1;
+      await post.save();
+    }
+
+    const posts = await Post.find({
+      tags: {$in: [...post.tags]},
+      _id: {$ne: post._id},
+    })
+      .sort({createdAt: "desc"})
+      .limit(5)
+      .select("-content");
+
+    const relatedPosts = posts.map(
+      ({_id, title, slug, tags, meta, thumbnail, createdAt}) => ({
+        id: _id.toString(),
+        title,
+        meta,
+        slug,
+        tags,
+        thumbnail: thumbnail?.url || "",
+        createdAt: createdAt.toString(),
+      })
+    );
+
+    const {
+      _id,
+      title,
+      meta,
+      content,
+      slug,
+      tags,
+      thumbnail,
+      createdAt,
+      views = 0,
+    } = post;
+
+    return {
+      id: _id.toString(),
+      title,
+      meta,
+      content,
+      slug,
+      tags,
+      thumbnail: thumbnail?.url || "",
+      createdAt: createdAt.toString(),
+      relatedPosts,
+      views,
+    }
+  } catch (error: any) {
+    return null
+  }
+};
 
 type Params = {
   slug: string;
@@ -23,7 +86,7 @@ export const generateStaticParams = async (): Promise<Params[]> => {
 
 export default async function PostPage({params}: Props) {
   const post = await getPost(params)
-  return (
+  if (post) return (
     <PostContent post={post}/>
   );
 }
