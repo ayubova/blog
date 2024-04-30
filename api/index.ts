dad80afe343c20188555e6f8f6111dc940b782be
Ayubova <ayubova.yuliya@gmail.com>
2024-04-30 11:13:30 +0400

    fix

--- api/index.ts
+++ api/index.ts
@@ -69,7 +69,7 @@ export const setLikeByPostId = (id: string) =>
 
 export const getTags = async () => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/posts/tags`);
-  console.log("response!!!!!!", response);
+  console.log("process.env.NEXT_PUBLIC_URL", process.env.NEXT_PUBLIC_URL);
   const data = await response.json();
   return data
 }
