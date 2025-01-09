3bf7252f24e6107e8a60a56d82de034830879303
Ayubova <ayubova.yuliya@gmail.com>
2025-01-09 20:33:13 +0100

    update about me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,10 +6,10 @@ const src =
   "https://res.cloudinary.com/dynf9cvqt/image/upload/v1678440784/blog/ffsggtpsxgtomq31o5ya.jpg";
 
 const About: NextPage = () => {
-  const yearsOfExperience  = new Date().getFullYear() - 2018
+  const yearsOfExperience = new Date().getFullYear() - 2018;
 
   return (
-    <div className="py-10 md:px-10 px-4 md:space-x-10 space-y-10 flex md:flex-row flex-col max-w-6xl">
+    <div className="py-10 md:px-10 px-4 md:space-x-10 space--10 flex md:flex-row flex-col max-w-6xl">
       <div className="w-full min-w-fit overflow-clip  md:mt-14">
         <Image
           src={src}
@@ -21,38 +21,34 @@ const About: NextPage = () => {
       </div>
       <div className="py-4 text-justify space-y-4 text-primary-dark dark:text-primary-light">
         <p className="font-semibold text-center md:text-lg font-heading">
-            Hello and welcome to my blog!
+          Hello and welcome to my blog!
         </p>
         <p>
           {`My name is Julia Ayubova, and I am a frontend developer with over ${yearsOfExperience}
             years of experience in the industry.`}
         </p>
         <p>
-            Originally from Moscow, Russia, I spent the last year living in
-            Alanya, Turkey, and my current location is in Yerevan, Armenia.
-            However, I guess this is not the final stop in my life.
+          Originally from Moscow, I’ve lived in Alanya, Turkey, and Yerevan,
+          Armenia, before settling in Amsterdam, Netherlands.
         </p>
         <p>
-            Aside from coding, I have a few hobbies that I enjoy. I love reading
-            books, and I try to read at least two books per month. I also enjoy
-            sports, especially going to the gym and running. I recently started
-            learning to play the piano, because it was my childhood dream. Also
-            I have three adorable cats who keep me company while I work from
-            home.
+          Beyond coding, I love reading and aim to finish at least two books per
+          month. Staying active is important to me, and I enjoy running, working
+          out, and recently started learning piano—a childhood dream of mine.
         </p>
         <p>
-            I started this blog as a way to share my experiences and insights
-            with others. I created this blog myself using Next.js.
+          In October 2023, I welcomed my son, Alan, into the world, and he has
+          become the most special part of my life.
         </p>
         <p>
-            One of the reasons I decided to start this blog is that I've quit
-            social media. I wanted to create a platform where I can express
-            myself without the distractions that come with social media.
+          I created this blog to share my experiences and insights. After
+          quitting social media, I built this space using Next.js to express
+          myself without the distractions of traditional platforms.
         </p>
         <p>
-            I hope you find my content useful and informative. If you have any
-            questions or comments, feel free to reach out to me. Let's connect
-            and learn together!
+          I hope you find my content helpful and inspiring. If you have any
+          questions or comments, feel free to reach out. Let’s connect and grow
+          together!
         </p>
       </div>
     </div>
